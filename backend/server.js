5f30e942c0a4a6dc88b320c1cbe792626a09a840
Mahesh7s <mokamahesh77@gmail.com>
2025-09-21 15:50:39 +0530

    commiting the frontend and backend code of podcasthub project

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(
   cors({
   origin: ["http://localhost:5173","https://podcasthub7.netlify.app/" ,"http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5177", "https://podcast-hub-project7.netlify.app"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
   })
 );
 app.use(express.json());

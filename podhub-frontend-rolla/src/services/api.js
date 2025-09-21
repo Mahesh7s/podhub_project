a8ce6e72ec39b454a7b5b711cf226c628163987c
Mahesh7s <mokamahesh77@gmail.com>
2025-09-21 13:07:06 +0530

    commiting the frontend and backend code of podcasthub project

--- podhub-frontend-rolla/src/services/api.js
+++ podhub-frontend-rolla/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // adjust if needed
+  baseURL: process.env.REACT_APP_API_URL||"http://localhost:3000/api", // adjust if needed
 });
 
 // Attach token automatically to every request
